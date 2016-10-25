d397a194ce5172809ead9aa16996908eea7e672b
Noah Udlis <noahudlis@comcast.net>
2016-10-24 22:24:43 -0700

    print date

--- src/firstnu/nuoverall.java
+++ src/firstnu/nuoverall.java
@@ -1,17 +1,21 @@
 package firstnu;
 
+import java.util.Date;
+
 /**
  * Created by Noah Udlis on 10/22/2016.
  */
 public class nuoverall {
     public static void main(String[] args)
     {
+
         System.out.println("Hello");
+        System.out.println(new Date());
     }
 
     public static void comedian()
     {
-        System.out.println("Knock knock.")
+        System.out.println("Knock knock");
     }
     public static void audience()
     {
