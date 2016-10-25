ae5684aa56f4542c2fd3f6db016a8e9916186793
Noah Udlis <noahudlis@comcast.net>
2016-10-24 22:16:36 -0700

    Giving some simple code

--- src/firstnu/nuoverall.java
+++ src/firstnu/nuoverall.java
@@ -4,4 +4,17 @@ package firstnu;
  * Created by Noah Udlis on 10/22/2016.
  */
 public class nuoverall {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+
+    public static void comedian()
+    {
+        System.out.println("Knock knock.")
+    }
+    public static void audience()
+    {
+        System.out.println("HAHAHAHHA");
+    }
 }
